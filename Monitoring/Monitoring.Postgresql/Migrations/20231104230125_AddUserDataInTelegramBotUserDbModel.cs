42431436622c5f82d7b4e90b0463c032dcda2a48
CMSPLANET\naumov <naumovandreyn@gmail.com>
2023-11-05 02:04:07 +0300

    add migration AddUserDataInTelegramBotUserDbModel

---
+++ Monitoring/Monitoring.Postgresql/Migrations/20231104230125_AddUserDataInTelegramBotUserDbModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Monitoring.Postgresql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserDataInTelegramBotUserDbModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "TelegramBotUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "TelegramBotUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "TelegramBotUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "TelegramBotUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "TelegramBotUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "TelegramBotUsers");
+        }
+    }
+}
